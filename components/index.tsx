44618f9f6e38100aa40e1935992010faf642a0b9
4FOURSEVEN7 <4FOURSEVEN7>
2023-07-23 05:09:38 +0000

    Pushed from Locofy Builder

---
+++ components/index.tsx
@@ -0,0 +1,108 @@
+import type { NextPage } from "next";
+
+const FrameComponent: NextPage = () => {
+  return (
+    <div className="relative [background:linear-gradient(175.3deg,_rgba(255,_0,_153,_0),_#4200ff)] shadow-[0px_5.423751354217529px_27.12px_13.56px_rgba(0,_0,_0,_0.25)] w-[589px] h-[1015px] overflow-hidden text-left text-[24.04px] text-white font-roboto">
+      <img
+        className="absolute top-[0px] left-[0px] w-[745.31px] h-[1061.07px]"
+        alt=""
+        src="/vector.svg"
+      />
+      <img
+        className="absolute top-[-178px] left-[-306px] w-[551px] h-[1061px] hidden"
+        alt=""
+        src="/vector1.svg"
+      />
+      <img
+        className="absolute top-[610.67px] left-[0px] w-[863.92px] h-[1359.19px]"
+        alt=""
+        src="/vector2.svg"
+      />
+      <img
+        className="absolute top-[0px] left-[0px] w-[820px] h-[1157px]"
+        alt=""
+        src="/vector3.svg"
+      />
+      <img
+        className="absolute top-[583.05px] left-[-376.75px] w-[586.7px] h-[835.26px] hidden"
+        alt=""
+        src="/vector4.svg"
+      />
+      <div className="absolute top-[920.02px] left-[0px] bg-white shadow-[0px_0px_3.21px_rgba(0,_0,_0,_0.5)] w-[572.21px] h-[84.95px] hidden" />
+      <div className="absolute top-[66px] left-[30px] w-[527.33px] h-[913.15px]">
+        <div className="absolute top-[84.95px] left-[1.6px] rounded-[8.01px] bg-cornflowerblue shadow-[0px_6.411269187927246px_6.41px_rgba(0,_0,_0,_0.5)] w-[525.72px] h-[84.95px]" />
+        <div className="absolute top-[190.74px] left-[1.6px] rounded-[8.01px] bg-white shadow-[0px_6.411269187927246px_6.41px_rgba(0,_0,_0,_0.5)] w-[525.72px] h-[84.95px]" />
+        <div className="absolute top-[296.52px] left-[1.6px] rounded-[8.01px] bg-white shadow-[0px_6.411269187927246px_6.41px_rgba(0,_0,_0,_0.5)] w-[525.72px] h-[84.95px]" />
+        <div className="absolute top-[884.76px] left-[344.61px] text-aqua [text-shadow:0px_1.6028172969818115px_0px_rgba(0,_0,_0,_0.25)]">
+          Sign up
+        </div>
+        <div className="absolute top-[883.15px] left-[73.73px] text-[25.65px] [text-shadow:0px_2px_6.41px_rgba(0,_0,_0,_0.4)]">
+          Don’t have an account?
+        </div>
+        <div className="absolute top-[772.56px] left-[344.61px] text-[20.84px] [text-shadow:0px_6.411269187927246px_6.41px_rgba(0,_0,_0,_0.5)]">
+          Forgot password?
+        </div>
+        <div className="absolute top-[108.99px] left-[78.54px] text-[30.72px] inline-block w-[370.39px] h-[61.43px] text-black font-roboto-flex">
+          <b>{` `}</b>
+          <span className="font-semibold text-white">
+            {" "}
+            Continue with Facebook
+          </span>
+        </div>
+        <div className="absolute top-[214.96px] left-[105.02px] text-[30.72px] text-black inline-block w-[370.39px] h-[61.43px] font-roboto-flex">
+          <b>{` `}</b>
+          <span className="font-semibold"> Continue with Google</span>
+        </div>
+        <div className="absolute top-[320.73px] left-[105.02px] text-[30.72px] text-black inline-block w-[370.39px] h-[61.43px] font-roboto-flex">
+          <b>{` `}</b>
+          <span className="font-semibold"> Continue with Apple</span>
+        </div>
+        <div className="absolute top-[435.16px] left-[295.72px] box-border w-[230.81px] h-[1.6px] opacity-[0.5] border-t-[1.6px] border-solid border-black" />
+        <div className="absolute top-[435.16px] left-[2.4px] box-border w-[230.81px] h-[1.6px] border-t-[1.6px] border-solid border-gray-400" />
+        <div className="absolute top-[416.73px] left-[246.83px] font-medium font-poppins [text-shadow:0px_0px_6.41px_rgba(255,_255,_255,_0.25)]">
+          OR
+        </div>
+        <div className="absolute top-[492.06px] left-[0px] rounded-[8.01px] bg-white box-border w-[525.72px] h-[70.52px] border-[2px] border-solid border-black" />
+        <div className="absolute top-[596.25px] left-[0px] rounded-[8.01px] bg-white box-border w-[525.72px] h-[70.52px] border-[2px] border-solid border-black" />
+        <div className="absolute top-[690.81px] left-[0px] rounded-[8.01px] bg-deeppink-100 shadow-[0px_6.411269187927246px_6.41px_2px_rgba(0,_0,_0,_0.4)] w-[525.72px] h-[70.52px]" />
+        <div className="absolute top-[512.9px] left-[32.06px] text-darkgray">
+          Email / Username
+        </div>
+        <div className="absolute top-[617.08px] left-[32.06px] text-darkgray">
+          Password
+        </div>
+        <img
+          className="absolute top-[675.07px] left-[49.23px] w-[52.2px] h-[46.19px] object-cover hidden"
+          alt=""
+          src="/password-1@2x.png"
+        />
+        <b className="absolute top-[708.45px] left-[222.79px] text-[29.75px] inline-block font-roboto-flex w-[100.98px] h-[35.26px]">
+          Log In
+        </b>
+        <img
+          className="absolute top-[323.77px] left-[27.25px] w-[31.24px] h-[31.24px] object-cover"
+          alt=""
+          src="/apple-logo-1@2x.png"
+        />
+        <img
+          className="absolute top-[217.98px] left-[27.25px] w-[34.21px] h-[31.24px] object-cover"
+          alt=""
+          src="/googlelogo-2@2x.png"
+        />
+        <img
+          className="absolute top-[112.2px] left-[30.45px] w-[31.24px] h-[31.24px] object-cover"
+          alt=""
+          src="/whitefblogo-1@2x.png"
+        />
+        <b className="absolute top-[0px] left-[78.54px] text-[35.24px] font-inter [text-shadow:0px_4px_20px_rgba(0,_0,_0,_0.25)] text-black">
+          <span>{`Sign-In to `}</span>
+          <span className="[background:linear-gradient(180deg,_#ff00e5_27.08%,_#00068f_88.54%)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
+            UGCPlanet
+          </span>
+        </b>
+      </div>
+    </div>
+  );
+};
+
+export default FrameComponent;
